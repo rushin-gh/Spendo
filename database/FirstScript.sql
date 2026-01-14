27f4da7cf1b2755bfc199279dca49cd367ffe2e9
rushin-gh <rushin.mailme@gmail.com>
2026-01-14 23:22:05 +0530

    Database file update

--- database/FirstScript.sql
+++ database/FirstScript.sql
@@ -1,3 +1,40 @@
 --create database SpendoDB;
---use SpendoDB;
+use SpendoDB;
+
+--select * from sys.tables
+--select * from INFORMATION_SCHEMA.TABLES
+
+--create login spendo_app_user with password = 'spendo_app_password', check_policy = on;
+--create user spendo_app_user for login spendo_app_user;
+
+--ALTER ROLE db_datareader ADD MEMBER spendo_app_user;
+--ALTER ROLE db_datawriter ADD MEMBER spendo_app_user;
+
+-- Source - https://stackoverflow.com/a
+-- Posted by Brijesh Kumar Tripathi
+-- Retrieved 2026-01-14, License - CC BY-SA 4.0
+
+--select
+--    'data source=' + @@servername +
+--    ';initial catalog=' + db_name() +
+--    case type_desc
+--        when 'WINDOWS_LOGIN' 
+--            then ';trusted_connection=true'
+--        else
+--            ';user id=' + suser_name() + ';password=<<YourPassword>>'
+--    end
+--    as ConnectionString
+--from sys.server_principals
+--where name = suser_name()
+--data source=Rushi\SQLEXPRESS;initial catalog=SpendoDB;trusted_connection=true
+
+
+-- Source - https://stackoverflow.com/a
+-- Posted by MarioDS, modified by community. See post 'Timeline' for change history
+-- Retrieved 2026-01-14, License - CC BY-SA 4.0
+--Data Source=Rushi\SQLEXPRESS;Initial Catalog=SpendoDB;User Id=spendo_app_user;Password=spendo_app_password
+
+
+
+ALTER ROLE db_owner ADD MEMBER migration_user;
 
