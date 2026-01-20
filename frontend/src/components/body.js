5a609691aa09365b4ee70c2c70db5f373a115b24
rushin-gh <rushin.mailme@gmail.com>
2026-01-20 22:54:18 +0530

    Front setup for fetching data from backend setup WIP

--- frontend/src/components/body.js
+++ frontend/src/components/body.js
@@ -1,12 +1,67 @@
+import { useEffect, useState } from "react";
 import { SPENDO_APP_BASE_URL } from "../config.js";
 
 const Body = () => {
-  console.log(SPENDO_APP_BASE_URL);
+  const [expenses, setExpenses] = useState([]);
+
+  useEffect(() => {
+    const loadExpenses = async () => {
+      try {
+        const expenses = await GetAllExpenses();
+        setExpenses(expenses);
+      } catch (err) {}
+    };
+
+    loadExpenses();
+    console.log(expenses);
+  }, []);
+
+  const GetAllExpenses = async () => {
+    const url = `${SPENDO_APP_BASE_URL}/api/expense/get`;
+
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application-json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Api thrown some error.");
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      console.log("Error");
+    }
+  };
+
+  if (expenses == []) {
+    return <div>Expenses are not loaded at.</div>;
+  }
 
   return (
     <div>
-      {SPENDO_APP_BASE_URL}
-      {/* <table></table> */}
+      <table>
+        <thead>
+          <tr>
+            <th>Sr</th>
+            <th>Title</th>
+            <th>Desc</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Help</td>
+            <td>No Help</td>
+            <td>100</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
