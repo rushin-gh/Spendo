b9578b854755233daeeb6210d5c5c811177ebfa9
rushin-gh <rushin.mailme@gmail.com>
2026-01-27 22:56:19 +0530

    UI - Update implementation WIP

--- frontend/src/components/Body/body.js
+++ frontend/src/components/Body/body.js
@@ -4,15 +4,16 @@ import ExpenseAdd from "./expenseAdd.js";
 
 const Body = () => {
   const [expenses, setExpenses] = useState([]);
+  const [editingExpense, setEditingExpense] = useState(null);
+
+  const loadExpenses = async () => {
+    try {
+      const expenses = await GetAllExpenses();
+      setExpenses(expenses);
+    } catch (err) {}
+  };
 
   useEffect(() => {
-    const loadExpenses = async () => {
-      try {
-        const expenses = await GetAllExpenses();
-        setExpenses(expenses);
-      } catch (err) {}
-    };
-
     loadExpenses();
   }, []);
 
@@ -38,6 +39,28 @@ const Body = () => {
     }
   };
 
+  const DeleteExpense = async (expId) => {
+    const url = `${SPENDO_APP_BASE_URL}/api/expense/delete/${expId}`;
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Api thrown some error.");
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      console.log("Error");
+    }
+  };
+
   if (expenses == null) {
     return <div id="body">Error while loading expenses</div>;
   } else if (expenses?.length == 0) {
@@ -45,11 +68,17 @@ const Body = () => {
   } else {
     return (
       <div id="body">
-        <ExpenseAdd />
+        <ExpenseAdd
+          editingExpense={editingExpense}
+          onSave={() => {
+            setEditingExpense(null);
+            loadExpenses();
+          }}
+        />
         <table>
           <thead>
             <tr>
-              <th>Sr</th>
+              {/* <th>Sr</th> */}
               <th>Title</th>
               <th>Desc</th>
               <th>Amount</th>
@@ -61,12 +90,12 @@ const Body = () => {
             {expenses.map((exp) => {
               return (
                 <tr key={exp.id}>
-                  <td>{exp.id}</td>
+                  {/* <td>{exp.id}</td> */}
                   <td>{exp.title}</td>
                   <td>{exp.description}</td>
                   <td>{exp.amount}</td>
-                  <td>U</td>
-                  <td>D</td>
+                  <td onClick={() => setEditingExpense(exp)}>U</td>
+                  <td onClick={() => DeleteExpense(exp.id)}>D</td>
                 </tr>
               );
             })}
--- frontend/src/components/Body/expenseAdd.js
+++ frontend/src/components/Body/expenseAdd.js
@@ -1,34 +1,102 @@
+import { useEffect, useState } from "react";
 import { SPENDO_APP_BASE_URL } from "../../config";
 
-const ExpenseAdd = () => {
-  const AddExpense = async () => {
-    // const expense = {
-    //   title: ,
-    //   description: ,
-    //   amount:
-    // }
+const ExpenseAdd = ({ editingExpense, onSave }) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
 
-    const url = SPENDO_APP_BASE_URL + "/api/expense/add";
+  // const AddExpense = async () => {
+  //   // Validation on client side
+  //   if (title == "" || description == "" || amount == "") {
+  //     return;
+  //   }
+
+  //   const expense = {
+  //     title: title,
+  //     description: description,
+  //     amount: amount,
+  //   };
+
+  //   const url = SPENDO_APP_BASE_URL + "/api/expense/add";
+  //   try {
+  //     const response = await fetch(url, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(expense),
+  //     });
+  //   } catch (error) {
+  //     console.log(error.Message);
+  //   }
+  // };
+
+  const handleSubmit = async () => {
+    if (!editingExpense && (title == "" || description == "" || amount == "")) {
+      return;
+    }
+
+    let expense = {};
+    if (title != "") expense.title = title;
+    if (description != "") expense.description = description;
+    if (amount != "") expense.amount = amount;
+
+    const url = editingExpense
+      ? `${SPENDO_APP_BASE_URL}/api/expense/update/${editingExpense.id}`
+      : `${SPENDO_APP_BASE_URL}/api/expense/add`;
+
+    const method = editingExpense ? "PUT" : "POST";
     try {
       const response = await fetch(url, {
-        method: "POST",
+        method: method,
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({}),
+        body: JSON.stringify(expense),
       });
     } catch (error) {
       console.log(error.Message);
     }
   };
 
+  useEffect(() => {
+    if (editingExpense) {
+      setTitle(editingExpense.title);
+      setDescription(editingExpense.description);
+      setAmount(editingExpense.amount);
+    } else {
+      setTitle("");
+      setDescription("");
+      setAmount("");
+    }
+  });
+
   return (
     <div id="expInp">
-      <input type="text" name="expTitle" placeholder="Title" />
-      <input type="text" name="expDesc" placeholder="Description" />
-      <input type="number" name="expAmt" placeholder="Amount" />
-      <button type="submit" onClick={AddExpense}>
-        Submit
+      <input
+        type="text"
+        name="expTitle"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        name="expdescription"
+        placeholder="description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <input
+        type="number"
+        name="expAmt"
+        placeholder="Amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <button type="submit" onClick={handleSubmit}>
+        {editingExpense ? "Update" : "Submit"}
       </button>
     </div>
   );
