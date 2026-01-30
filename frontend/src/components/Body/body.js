3f058b3e812f46bcb0928cb163d4997ca4a96680
rushin-gh <rushin.mailme@gmail.com>
2026-01-30 23:52:44 +0530

    Frontend Refactoring in progress

--- frontend/src/components/Body/body.js
+++ frontend/src/components/Body/body.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import ExpenseAdd from "./expenseAdd.js";
+import { GetAllExpenses, DeleteExpense } from "../../utils/expenseFunctions";
+import ExpenseAdd from "./expenseAdd";
 
 const Body = () => {
   const [expenses, setExpenses] = useState([]);
@@ -9,57 +10,58 @@ const Body = () => {
     try {
       const expenses = await GetAllExpenses();
       setExpenses(expenses);
-    } catch (err) {}
+    } catch (err) {
+      console.log(err.message);
+      // Some error logging functionality
+    }
   };
 
   useEffect(() => {
     loadExpenses();
   }, []);
 
-  if (expenses == null) {
-    return <div id="body">Error while loading expenses</div>;
-  } else if (expenses?.length == 0) {
-    return <div id="body">No expenses</div>;
-  } else {
-    return (
-      <div id="body">
-        <ExpenseAdd
-          editingExpense={editingExpense}
-          setEditingExpense={setEditingExpense}
-          onSave={() => {
-            setEditingExpense(null);
-            loadExpenses();
-          }}
-        />
-        <table>
-          <thead>
-            <tr>
-              {/* <th>Sr</th> */}
-              <th>Title</th>
-              <th>Desc</th>
-              <th>Amount</th>
-              <th>Update</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {expenses.map((exp) => {
-              return (
-                <tr key={exp.id}>
-                  {/* <td>{exp.id}</td> */}
-                  <td>{exp.title}</td>
-                  <td>{exp.description}</td>
-                  <td>{exp.amount}</td>
-                  <td onClick={() => setEditingExpense(exp)}>U</td>
-                  <td onClick={() => DeleteExpense(exp.id)}>D</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+  return !expenses ? (
+    <div id="body">Error while loading expenses</div>
+  ) : expenses.length == 0 ? (
+    <div id="body">No expenses</div>
+  ) : (
+    <div id="body">
+      <ExpenseAdd
+        editingExpense={editingExpense}
+        setEditingExpense={setEditingExpense}
+        onSave={() => {
+          setEditingExpense(null);
+          loadExpenses();
+        }}
+      />
+      <table>
+        <thead>
+          <tr>
+            {/* <th>Sr</th> */}
+            <th>Title</th>
+            <th>Desc</th>
+            <th>Amount</th>
+            <th>Update</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((exp) => {
+            return (
+              <tr key={exp.id}>
+                {/* <td>{exp.id}</td> */}
+                <td>{exp.title}</td>
+                <td>{exp.description}</td>
+                <td>{exp.amount}</td>
+                <td onClick={() => setEditingExpense(exp)}>U</td>
+                <td onClick={() => DeleteExpense(exp.id)}>D</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Body;
--- frontend/src/components/Body/expenseAdd.js
+++ frontend/src/components/Body/expenseAdd.js
@@ -1,64 +1,35 @@
 import { useEffect, useState } from "react";
-import { SPENDO_APP_BASE_URL } from "../../config";
+import { AddExpense, UpdateExpense } from "../../utils/expenseFunctions";
 
 const ExpenseAdd = ({ editingExpense, setEditingExpense, onSave }) => {
-  console.log("ExpenseAdd component called.");
-  console.log(editingExpense);
-
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [amount, setAmount] = useState("");
 
-  const handleSubmit = async () => {
-    if (!editingExpense && (title == "" || description == "" || amount == "")) {
-      return;
-    }
-
-    let expense = {};
-    if (title != "") expense.title = title;
-    if (description != "") expense.description = description;
-    if (amount != "") expense.amount = amount;
-
-    const url = editingExpense
-      ? `${SPENDO_APP_BASE_URL}/api/expense/update/${editingExpense.id}`
-      : `${SPENDO_APP_BASE_URL}/api/expense/add`;
-
-    const method = editingExpense ? "PATCH" : "POST";
-    try {
-      const response = await fetch(url, {
-        method: method,
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(expense),
-      });
-    } catch (error) {
-      console.log(error.Message);
-    }
-
-    onSave();
-  };
-
-  const handleInputs = () => {
-    console.log(editingExpense);
-    if (editingExpense) {
-      setTitle(editingExpense.title);
-      setDescription(editingExpense.description);
-      setAmount(editingExpense.amount);
-    } else {
-      setTitle("");
-      setDescription("");
-      setAmount("");
-    }
-  };
-
   useEffect(() => {
-    console.log("ExpenseAdd useEffect called.");
     handleInputs();
   }, [editingExpense]);
 
-  const handleCancel = () => {
-    setEditingExpense(null);
+  const handleSubmit = async (exp, expId) => {
+    if (editingExpense) {
+      UpdateExpense(expId, exp);
+    } else {
+      AddExpense(exp);
+    }
+  };
+
+  const handleInputs = () => {
+    var values = {
+      title: editingExpense ? editingExpense.title : "",
+      description: editingExpense ? editingExpense.description : "",
+      amount: editingExpense ? editingExpense.amount : "",
+    };
+
+    if (editingExpense) {
+      setTitle(values.title);
+      setDescription(values.description);
+      setAmount(values.amount);
+    }
   };
 
   return (
